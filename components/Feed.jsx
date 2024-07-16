bf212e529d1123d4e20e3b3b2a0db2b37640276f
imfarhat <ifarhat.dev@gmail.com>
2024-07-16 23:15:28 +0530

    feat; add search functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -19,33 +19,75 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
-  const handleSearchChange = (e) => {};
+  const [filteredPosts, setFilteredPosts] = useState([]);
 
-  
+  const handleSearchChange = async (e) => {
+    const searchText = e.target.value;
+    setSearchText(searchText);
+
+    if (searchText === "") {
+      setFilteredPosts(posts); // Display all posts when search text is empty
+    } else {
+      const filtered = await filterPosts(searchText);
+      setFilteredPosts(filtered);
+    }
+  };
   useEffect(() => {
     const fetchPost = async () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
       setPosts(data);
+      setFilteredPosts(data);
     };
     fetchPost();
   }, []);
 
+  const filterPosts = async (searchText) => {
+    const regex = new RegExp(searchText, "i"); // "i" flag for case-sensetive search
+
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleTagClick = async (tag) => {
+    setSearchText(tag);
+    const filtered = await filterPosts(tag);
+    setFilteredPosts(filtered);
+  };
+
   return (
     <section className="feed">
-      <form action="" className="relative w-full flex-center">
+      <form action="#" className="relative w-full flex-center">
         <input
           type="text"
           placeholder="Search for a tag or a username"
           value={searchText}
           onChange={handleSearchChange}
-          name=""
-          id=""
+          name="search-text"
+          id="search-text"
           required
           className="search_input peerr"
         />
+
+        {searchText && (
+          <label
+            htmlFor="search-text"
+            className="absolute right-3 text-xs sm:text-sm font-inter text-gray-500 cursor-pointer select-none"
+            onClick={() => {
+              if (!searchText) return;
+              setSearchText("");
+              setFilteredPosts(posts);
+            }}
+          >
+            Reset
+          </label>
+        )}
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      <PromptCardList data={filteredPosts} handleTagClick={handleTagClick} />
     </section>
   );
 };
